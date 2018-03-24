9fa047486bce7d0dab24338920a729f0ae2cbf64
Rostyslav <rostyslav@Rostyslavs-MacBook-Pro.local>
2018-03-24 13:07:16 +0100

    try some code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Enable actions client library debugging
 process.env.DEBUG = 'actions-on-google:*';
+// https://us-central1-sillynamemaker-e3a42.cloudfunctions.net/sillyNameMaker
 
 let App = require('actions-on-google').DialogflowApp;
 let express = require('express');
@@ -11,8 +12,8 @@ let app = express();
 app.set('port', (process.env.PORT || 8080));
 app.use(bodyParser.json({type: 'application/json'}));
 
-const GENERATE_ANSWER_ACTION = 'generate_answer';
-const CHECK_GUESS_ACTION = 'check_guess';
+// const GENERATE_ANSWER_ACTION = 'generate_answer';
+// const CHECK_GUESS_ACTION = 'check_guess';
 
 app.use(morgan('dev'));
 
@@ -20,12 +21,61 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/app/index.html'));
 });
 
+
+// a. the action name from the make_name Dialogflow intent
+const WELCOME_INTENT = 'input.welcome';  // the action name from the Dialogflow intent
+const NAME_ACTION = 'make_name';
+const GAME_ACTION = 'game_action';
+// b. the parameters that are parsed from the make_name intent
+const COLOR_ARGUMENT = 'color';
+const NUMBER_ARGUMENT = 'number';
+
+const GAME_ARGUMENT = 'game';
+
 app.post('/', function (request, response) {
     console.log('headers: ' + JSON.stringify(request.headers));
     console.log('body: ' + JSON.stringify(request.body));
 
-    const app = new App({request: request, response: response});
-    response.sendStatus(200); // OK
+    const app = new App({request, response});
+    console.log('Request headers: ' + JSON.stringify(request.headers));
+    console.log('Request body: ' + JSON.stringify(request.body));
+
+
+    function welcomeIntentQuestion (app) {
+        app.ask(`Hi Dude! Welcome to Silly Name Maker! Let's get started. What is your lucky number?`,
+            ['I didn\'t hear a number', 'If you\'re still there, what\'s your lucky number?',
+                'We can stop here. Let\'s play again soon. Bye!']);
+    }
+
+// c. The function that generates the silly name
+    function makeName (app) {
+        let number = app.getArgument(NUMBER_ARGUMENT);
+        let color = app.getArgument(COLOR_ARGUMENT);
+        app.ask(`Alright! Your super silly name is "${color} ${number}"! I hope you like it. Which kind of sport do you like to play?`);
+    }
+
+    function createSmthWithGame (app) {
+        let context = app.getContext('namecreated');
+
+        const game = app.getContextArgument(context.name, GAME_ARGUMENT);
+
+        const color = context.parameters.color;
+        const number = context.parameters.number;
+
+
+        app.tell(`Wazuuup "${color} ${number}"!!! I like also to play your ${game.original}`);
+    }
+
+    // d. build an action map, which maps intent names to functions
+    let actionMap = new Map();
+
+    actionMap.set(WELCOME_INTENT, welcomeIntentQuestion);
+    actionMap.set(NAME_ACTION, makeName);
+    actionMap.set(GAME_ACTION, createSmthWithGame);
+
+    app.handleRequst(actionMap);
+
+    // response.sendStatus(200); // OK
 });
 
 // Start the server
